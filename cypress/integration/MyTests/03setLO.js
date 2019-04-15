c9d295cb0e62747b7c3115905c792e5270952806
ignatas <ignatenkodg@gmail.com>
2019-04-15 18:41:26 +0300

    added fixture usage

--- cypress/integration/MyTests/03setLO.js
+++ cypress/integration/MyTests/03setLO.js
@@ -1,18 +1,20 @@
 const apikey = 'a18a2544-c6fb-4877-be5a-3fdad40819fd';
 let limitOrderId;
+let singleLO;
 //--Preparation--//
 it('autorized user1 check BTC balance', function() {
+    
     cy
     .request({
      url: '/wallets', // check balance for reserved amount
      headers: {'api-key': apikey},
     failOnStatusCode: false
-    })
+    })    
     .then((wallets) => {
-    expect(wallets.status).to.eq(200);
-    wallets.body.forEach(asset => {
-    if ( asset.AssetId ==='BTC' && asset.Reserved > 0) //if there are placed orders that reserve some amount
-    {
+        expect(wallets.status).to.eq(200);
+        wallets.body.forEach(asset => {
+        if ( asset.AssetId ==='BTC' && asset.Reserved > 0) //if there are placed orders that reserve some amount
+        {
         cy
         .request({
             url: '/Orders', // cancell all orders
@@ -21,58 +23,63 @@ it('autorized user1 check BTC balance', function() {
         })
         .then((orders) => {
         expect(orders.status).to.eq(200)}) //unnecessary. just flag
-    }
+        }
+   
    // if (asset.AssetId ==='BTC'){ expect(asset.Reserved).to.eq(0) }
-   })
+        })
     })      
-  })
+})
+
 //--Single limit order placement test--//
-  it('autorized user1 place LO BTCUSD Sell', function() {
+it('nn', function() {
+    cy.fixture('singleLO').then((data) => {singleLO = data})
+})
+
+it('autorized user1 place LO BTCUSD Sell', function() {
+    
     cy
     .request({
      url: '/orders/v2/limit', // place limit order
      headers: {'api-key': apikey},
      method: 'POST',
-     body: 
-     {
-        "AssetPairId": "BTCCHF",
-        "OrderAction": "Sell",
-        "Volume": 1.23456789,
-        "Price": 15000
-     }
-    })
-    
+     body: singleLO
+    })    
     .then((limit) => {
-    expect(limit.status).to.eq(200);
-    expect(limit.body).to.have.property('Id');
-limitOrderId = limit.body.Id;
-cy.request('/orderbooks');
-cy.request('/orderbooks'); 
-cy.request('/orderbooks'); // wait ~2-3 sec
-cy.request({
-    url: '/orders/'+limitOrderId, //check limit order info  by ID
-    headers: {'api-key': apikey},
-})
-.then((ordersbyid) => {expect(ordersbyid.status).to.eq(200);
-expect(ordersbyid.body).to.have.property('Id').eq(limitOrderId);
-expect(ordersbyid.body).to.have.property('Status').eq('Placed') //The LO is in orderbook
-})
-                    })
-//--Checking if balance is reserved by the placed order--//                    
-cy
-    .request({
-     url: '/wallets', // check balance for reserved amount
-     headers: {'api-key': apikey},
-    failOnStatusCode: false
+        expect(limit.status).to.eq(200);
+        expect(limit.body).to.have.property('Id');
+        limitOrderId = limit.body.Id;
+        
+        cy.request('/orderbooks');
+        cy.request('/orderbooks'); 
+        cy.request('/orderbooks'); // wait ~2-3 sec
+        cy.request({
+            url: '/orders/'+limitOrderId, //check limit order info  by ID
+            headers: {'api-key': apikey},
+        })
+        .then((ordersbyid) => {expect(ordersbyid.status).to.eq(200);
+            expect(ordersbyid.body).to.have.property('Id').eq(limitOrderId);
+            expect(ordersbyid.body).to.have.property('Status').eq('Placed'); //The LO is in orderbook
+            expect(ordersbyid.body).to.have.property('AssetPairId').eq(singleLO.AssetPairId); //The assetpair is correct
+            expect(ordersbyid.body).to.have.property('Price').eq(singleLO.Price);//The price is correct
+            if (singleLO.OrderAction === 'Sell') {expect(ordersbyid.body).to.have.property('Volume').eq(-singleLO.Volume)} //The volume and direction is correct
+        })
     })
-    
+
+    //--Checking if balance is reserved by the placed order--//                    
+    cy
+    .request({
+        url: '/wallets', // check balance for reserved amount
+        headers: {'api-key': apikey},
+        failOnStatusCode: false
+    })
     .then((wallets) => {
-    expect(wallets.status).to.eq(200);
-    wallets.body.forEach(asset => {
-    if (asset.AssetId ==='BTC'){ expect(asset.Reserved).to.eq(1.23456789) } 
-}) 
-})                    
+        expect(wallets.status).to.eq(200);
+        wallets.body.forEach(asset => {
+        if (asset.AssetId ==='BTC'){ expect(asset.Reserved).to.eq(singleLO.Volume) } 
+        }) 
+    })                    
 })
+
 //--The single limit order cancelation test--//
 it('autorized user1 cancel the LO', function() {
     cy
@@ -82,18 +89,19 @@ it('autorized user1 cancel the LO', function() {
         headers: {'api-key': apikey},
     })
     .then((orders) => {
-    expect(orders.status).to.eq(200)})
+        expect(orders.status).to.eq(200)
+    })
         
     cy
     .request({
-     url: '/wallets', // check balance for reserved amount
-     headers: {'api-key': apikey},
-    failOnStatusCode: false
+            url: '/wallets', // check balance for reserved amount
+            headers: {'api-key': apikey},
+            failOnStatusCode: false
     })
     .then((wallets) => {
-    expect(wallets.status).to.eq(200);
-    wallets.body.forEach(asset => {
-    if (asset.AssetId ==='BTC'){ expect(asset.Reserved).to.eq(0) }//Should be 0
-   })
+        expect(wallets.status).to.eq(200);
+         wallets.body.forEach(asset => {
+         if (asset.AssetId ==='BTC'){ expect(asset.Reserved).to.eq(0) }//Should be 0
+        })
     })      
 }) 
