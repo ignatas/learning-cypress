14680dfe5bf53c7c2b4a8de969df4cecdfc14e54
ignatas <ignatenkodg@gmail.com>
2019-04-22 11:47:30 +0300

    Update katetask1.js

--- cypress/integration/Kate_tasks/katetask1.js
+++ cypress/integration/Kate_tasks/katetask1.js
@@ -1,11 +1,12 @@
 import Chance from 'chance'
 describe('task1', () => {
-    let product;
+    let product ='';
     before('', () => {
         cy.request('https://storage.googleapis.com/mannequin/2018/data/productwall/accessories/en_us.json')
             .then((response) => {
                 let number = Chance().integer({ min: 0, max: response.body.products.length }) //random product #
                 product = response.body.products[number].display_name
+                if (product.indexOf(' -') >0){product = product.substring(0,product.indexOf(' -'))}
             })
     })
 
@@ -17,6 +18,8 @@ describe('task1', () => {
             .click()
         cy.get('.quantumWizAutocompleteInputText')
             .type(product + '{enter}')
+        cy.get(':nth-child(1) > .card-link-target > .card-inner')
+        .should('exist')
 
     })
 
