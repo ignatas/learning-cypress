803b5330757b33ca89d0f2e2f7cca18065eced86
ignatas <ignatenkodg@gmail.com>
2019-04-23 17:22:33 +0300

    task 1 complete

--- cypress/integration/Kate_tasks/katetask1.js
+++ cypress/integration/Kate_tasks/katetask1.js
@@ -1,26 +1,27 @@
 import Chance from 'chance'
 describe('task1', () => {
-    let product ='';
+    let product = {
+        "name": "",
+        "url": ""
+    };
     before('', () => {
         cy.request('https://storage.googleapis.com/mannequin/2018/data/productwall/accessories/en_us.json')
             .then((response) => {
                 let number = Chance().integer({ min: 0, max: response.body.products.length }) //random product #
-                product = response.body.products[number].display_name
-                if (product.indexOf(' -') >0){product = product.substring(0,product.indexOf(' -'))}
+                product.name = response.body.products[number].display_name
+                product.url = response.body.products[number].url
+                if (product.name.indexOf(' -') > 0) { product.name = product.name.substring(0, product.name.indexOf(' -')) }
             })
     })
 
-
-
-    it('', () => {
+    it('positive : search the existing product', () => {
         cy.visit('store.google.com/us/?hl=en-US&countryRedirect=true')
-        cy.get('.header-search-icon > .highlightable > svg')
+        cy.get('.header-search-icon > .highlightable > svg', { timeout: 20000 })
             .click()
         cy.get('.quantumWizAutocompleteInputText')
-            .type(product + '{enter}')
-        cy.get(':nth-child(1) > .card-link-target > .card-inner')
-        .should('exist')
-
+            .type(product.name + '{enter}')
+        // check if the product is really found
+        cy.get('input[value="' + product.url + '"]', { timeout: 20000 }).should('exist')
     })
 
 })
