3cb16300343434147c6e1fb812ed2bd0a0f67c9e
ignatas <ignatenkodg@gmail.com>
2019-04-26 13:10:42 +0300

    2

--- cypress/integration/Kate_tasks/katetask3.js
+++ cypress/integration/Kate_tasks/katetask3.js
@@ -5,23 +5,30 @@ import commonPageActions from "../../support/Objects/commonPageActions"
 import pageSearch from '../../support/Objects/pageSearch';
 
 
-before('preparation : add random product to cart',()=>{
-    
-    commonPageActions.pickRandomProduct()
-    
-    commonPageActions.addProductToCart()
+before('preparation : add random product to cart', () => {
 
+    commonPageActions.pickRandomProduct()
+
+    commonPageActions.addProductToCart()
+    cy.wait(5000)
 
 })
 
-it('positive : product qnt change',()=>{
-    
+it('positive : product qnt change', () => {
+
     cy.visit('https://store.google.com/us/cart?hl=en-US')
-    
-
-        pageCart.getProductQuantity().then((text) => { expect(text).to.eq("1") })//check : the only one item in the cart
-
-        pageCart.getTotalPrice().then((text) => { expect(text).to.eq(pageCart.getProductPrice()) })//check : the total price is correct
 
 
+    pageCart.getProductQuantity().then((text) => { expect(text).to.eq("1") })//check : the only one item in the cart
+
+    pageCart.getTotalPrice().then((totalPrice) => {
+        pageCart.getProductPrice().then((productPrice) => {
+            expect(totalPrice).to.eq(productPrice)
+        })
+    })//check : the total price is correct
+
+    pageCart.setProductQuantity()//change qty
+
+    pageCart.removeProduct() //clear the cart for new test
+        .should('exist')//check : if the product is removed
 })
